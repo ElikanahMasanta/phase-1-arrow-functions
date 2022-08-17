0f3ddccb23f41f81d3acbb27b139b14f5ac29609
ElikanahMasanta <emasanta8@gmail.com>
2022-08-17 23:59:33 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(number, number1){
+    return 20;
+}
+divides(2000,100)
+
+const square = (nummber,number1) => (x ** 2); 
+square;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3);
